6800d0394638f18ee30854acb52a5717480aaa50
RAJAN SHARMA <rajan.kumar@geeky.dev>
2023-06-08 15:41:25 +0530

    first commit

---
+++ assets/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./scss/main.scss";
+import App from "./js/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./js/store/AuthContext";
+
+ReactDOM.render(
+	<AuthContextProvider>
+		<React.StrictMode>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</React.StrictMode>
+	</AuthContextProvider>,
+	document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
