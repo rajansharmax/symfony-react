1780e12643cbc1f811209d20b2c67417f6f32e93
RAJAN SHARMA <rajan.kumar@geeky.dev>
2023-07-09 22:34:54 +0530

    added navbar

--- assets/js/components/Layouts/Apple.tsx
+++ assets/js/components/Layouts/Apple.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from "react";
 import Header from "./Partials/Header";
 import Footer from "./Partials/Footer";
 import Sidebar from "./Partials/Sidebar";
+import Navbar from "./Partials/Navbar";
 
 interface LayoutProps {
     children: ReactElement;
@@ -10,9 +11,14 @@ const Apple = (props:LayoutProps) => {
     return (
         <React.Fragment>
             <Header />
-            <Sidebar />
-                {/* props = yeild */}
-                <main className="">{props.children}</main>
+            {/* <!-- Layout container --> */}
+                <div className="layout-page">
+                    {/* <!-- Navbar -->*/}
+                        <Sidebar />
+                        <Navbar />
+                        {/* props = yeild */}
+                        <main className="">{props.children}</main>
+                </div>
             <Footer />
         </React.Fragment>
     );
