72a65d02daeedde262dc9241a357eefe0de4c3b0
RAJAN SHARMA <rajan.kumar@geeky.dev>
2023-07-22 23:29:14 +0530

    working in permissions

--- assets/js/pages/Admin/Permissions.tsx
+++ assets/js/pages/Admin/Permissions.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Apple from "../../components/Layouts/Apple";
+import DataTable from "react-data-table-component";
+import { Card } from "reactstrap";
 
-const Permissions = () => {
+
+const Permissions: React.FC = () => {
   return (
     <Apple>
       {/* <!-- Content --> */}
       <div className="container-xxl flex-grow-1 container-p-y">
-        <div className="row"></div>
+          <h3>Permissions List</h3>
+          <p>
+            Each category (Basic, Professional, and Business) includes the four
+            predefined roles shown below.
+          </p>
+          <Fragment>
+            <Card>
+              <div className="card-datatable app-user-list table-responsive">
+                
+              </div>
+            </Card>
+          </Fragment>
       </div>
       {/* <!-- / Content --> */}
     </Apple>
